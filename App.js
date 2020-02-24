50d458ba64a02160b11aa753b50ab821e1055368
Rob <amigo@quo.wtf>
2020-02-23 19:03:12 -0600

    chore: add textinput

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Hola React Native</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "grey",
+            borderWidth: 1
+          }}
+        />
       </View>
     );
   }
