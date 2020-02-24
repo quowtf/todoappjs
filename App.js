c6297c00696e494bc4fc11724ff2676454a23b37
Rob <amigo@quo.wtf>
2020-02-23 18:56:38 -0600

    chore: add some basic styles

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 class App extends React.Component {
   render() {
-    return(
-      <View>
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Hola React Native</Text>
       </View>
-    )
+    );
   }
 }
 
