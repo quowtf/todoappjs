07cdb155b9183a3bd9d257d862dc03e74ec75faf
Rob <amigo@quo.wtf>
2020-02-23 18:32:44 -0600

    chore: initial commit, todoapp

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>Hola React Native</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
