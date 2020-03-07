77d473a0a80e91e895ea46fc2b1dd1c6049c476c
Rob <amigo@quo.wtf>
2020-03-07 13:16:32 -0600

    feat: dinamic add todo

--- App.js
+++ App.js
@@ -4,10 +4,17 @@ import { View, Text, TextInput, Button } from "react-native";
 class App extends React.Component {
   state = {
     text: "",
-    todo: ""
+    todo: []
   };
   addTodo = () => {
-    this.setState({ todo: this.state.text });
+    newTodo = this.state.todo;
+    newTodo.push(this.state.text);
+    this.setState({ todo: newTodo });
+  };
+  renderTodos = () => {
+    return this.state.todo.map(t => {
+      return <Text key={t}>{t}</Text>;
+    });
   };
   render() {
     return (
@@ -18,7 +25,7 @@ class App extends React.Component {
           onChangeText={newText => this.setState({ text: newText })}
         />
         <Button title="Add Todo" color="grey" onPress={this.addTodo} />
-        <Text>{this.state.todo}</Text>
+        {this.renderTodos()}
       </View>
     );
   }
