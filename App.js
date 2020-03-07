dfa621cb077c539f70de71f0dd88e40248e7b96a
Rob <amigo@quo.wtf>
2020-03-07 12:05:45 -0600

    feat: move styles to const & dinamic text

--- App.js
+++ App.js
@@ -2,21 +2,35 @@ import React from "react";
 import { View, Text, TextInput, Button } from "react-native";
 
 class App extends React.Component {
+  state = {
+    todo: "Este es el texto que cambia"
+  };
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.viewStyles}>
         <Text>Hola React Native</Text>
         <TextInput
-          style={{
-            height: 40,
-            borderColor: "grey",
-            borderWidth: 1
-          }}
+          style={styles.inputStyles}
+          onChangeText={newText => this.setState({ todo: newText })}
         />
         <Button title="Add Todo" color="grey" />
+        <Text>{this.state.todo}</Text>
       </View>
     );
   }
 }
+
+const styles = {
+  viewStyles: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputStyles: {
+    height: 40,
+    borderColor: "grey",
+    borderWidth: 1
+  }
+};
 
 export default App;
